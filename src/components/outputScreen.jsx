8c736591b4a269de99d80323b0ec3a8bee2e6e60
AbelG101 <abelgebeyehu101@gmail.com>
2022-09-07 01:56:45 +0300

    add output screen component

---
+++ src/components/outputScreen.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class OutputScreen extends Component {
+  state = {};
+  render() {
+    return (
+      <div class="output-screen-container">
+        <input class="output-screen" type="text" />
+      </div>
+    );
+  }
+}
+
+export default OutputScreen;
