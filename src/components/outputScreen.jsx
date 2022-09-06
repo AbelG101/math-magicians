ec6c803fc5521bbd3492470aaec651789df91820
AbelG101 <abelgebeyehu101@gmail.com>
2022-09-06 23:08:06 +0300

    add output field

---
+++ src/components/outputScreen.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class OutputScreen extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="output-screen-container">
+        <input className="output-screen" type="text" />
+      </div>
+    );
+  }
+}
+
+export default OutputScreen;
