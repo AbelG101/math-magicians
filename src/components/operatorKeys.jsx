b50bef4e66041a4f8077a065cf1054e91a10cf89
AbelG101 <abelgebeyehu101@gmail.com>
2022-09-07 18:38:47 +0300

    modify division key to be ÷

--- src/components/operatorKeys.jsx
+++ src/components/operatorKeys.jsx
@@ -13,7 +13,7 @@ class OperatorKeys extends Component {
         <button onClick={handleEvent} type="button" className="btn btn-top">AC</button>
         <button onClick={handleEvent} type="button" className="btn btn-top">+/-</button>
         <button onClick={handleEvent} type="button" className="btn btn-top">%</button>
-        <button onClick={handleEvent} type="button" className="btn btn-side">/</button>
+        <button onClick={handleEvent} type="button" className="btn btn-side">÷</button>
         <button onClick={handleEvent} type="button" className="btn btn-side">x</button>
         <button onClick={handleEvent} type="button" className="btn btn-side">-</button>
         <button onClick={handleEvent} type="button" className="btn btn-side">+</button>
