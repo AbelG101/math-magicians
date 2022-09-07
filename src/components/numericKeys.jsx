1454180717def4bef19aaec5955561c79dfb5897
AbelG101 <abelgebeyehu101@gmail.com>
2022-09-07 17:18:41 +0300

    add SyntheticEvent for numeric keys

--- src/components/numericKeys.jsx
+++ src/components/numericKeys.jsx
@@ -7,19 +7,20 @@ class NumericKeys extends Component {
   }
 
   render() {
+    const { handleEvent } = this.props;
     return (
       <div type="button" className="numerals">
-        <button type="button" className="btn btn-numerals">7</button>
-        <button type="button" className="btn btn-numerals">8</button>
-        <button type="button" className="btn btn-numerals">9</button>
-        <button type="button" className="btn btn-numerals">4</button>
-        <button type="button" className="btn btn-numerals">5</button>
-        <button type="button" className="btn btn-numerals">6</button>
-        <button type="button" className="btn btn-numerals">1</button>
-        <button type="button" className="btn btn-numerals">2</button>
-        <button type="button" className="btn btn-numerals">3</button>
-        <button type="button" className="btn btn-numerals col-span-2">0</button>
-        <button type="button" className="btn btn-numerals">.</button>
+        <button onClick={handleEvent} type="button" className="btn btn-numerals">7</button>
+        <button onClick={handleEvent} type="button" className="btn btn-numerals">8</button>
+        <button onClick={handleEvent} type="button" className="btn btn-numerals">9</button>
+        <button onClick={handleEvent} type="button" className="btn btn-numerals">4</button>
+        <button onClick={handleEvent} type="button" className="btn btn-numerals">5</button>
+        <button onClick={handleEvent} type="button" className="btn btn-numerals">6</button>
+        <button onClick={handleEvent} type="button" className="btn btn-numerals">1</button>
+        <button onClick={handleEvent} type="button" className="btn btn-numerals">2</button>
+        <button onClick={handleEvent} type="button" className="btn btn-numerals">3</button>
+        <button onClick={handleEvent} type="button" className="btn btn-numerals col-span-2">0</button>
+        <button onClick={handleEvent} type="button" className="btn btn-numerals">.</button>
       </div>
     );
   }
