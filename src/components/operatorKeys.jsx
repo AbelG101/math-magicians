eee0ea972993fbe2e8f8376616b472fff122fccb
AbelG101 <abelgebeyehu101@gmail.com>
2022-09-07 21:32:50 +0300

    refactored OperatorKeys.jsx to use hooks

--- src/components/operatorKeys.jsx
+++ src/components/operatorKeys.jsx
@@ -1,26 +1,16 @@
-import React, { Component } from 'react';
+import { React } from 'react';
 
-class OperatorKeys extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const { handleEvent } = this.props;
-    return (
-      <>
-        <button onClick={handleEvent} type="button" className="btn btn-top">AC</button>
-        <button onClick={handleEvent} type="button" className="btn btn-top">+/-</button>
-        <button onClick={handleEvent} type="button" className="btn btn-top">%</button>
-        <button onClick={handleEvent} type="button" className="btn btn-side">÷</button>
-        <button onClick={handleEvent} type="button" className="btn btn-side">x</button>
-        <button onClick={handleEvent} type="button" className="btn btn-side">-</button>
-        <button onClick={handleEvent} type="button" className="btn btn-side">+</button>
-        <button onClick={handleEvent} type="button" className="btn btn-side">=</button>
-      </>
-    );
-  }
-}
+const OperatorKeys = ({ handleEvent }) => (
+  <>
+    <button onClick={handleEvent} type="button" className="btn btn-top">AC</button>
+    <button onClick={handleEvent} type="button" className="btn btn-top">+/-</button>
+    <button onClick={handleEvent} type="button" className="btn btn-top">%</button>
+    <button onClick={handleEvent} type="button" className="btn btn-side">÷</button>
+    <button onClick={handleEvent} type="button" className="btn btn-side">x</button>
+    <button onClick={handleEvent} type="button" className="btn btn-side">-</button>
+    <button onClick={handleEvent} type="button" className="btn btn-side">+</button>
+    <button onClick={handleEvent} type="button" className="btn btn-side">=</button>
+  </>
+);
 
 export default OperatorKeys;
