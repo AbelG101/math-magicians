97ee47c010bf8088817e3aae75834cf9fe5ce516
AbelG101 <abelgebeyehu101@gmail.com>
2022-09-13 01:32:12 +0300

    add Quote page component

--- src/pages/Quote.jsx
+++ src/pages/Quote.jsx
@@ -2,20 +2,6 @@ import React from 'react';
 
 const Quote = () => (
   <>
-    <nav className="nav-container">
-      <h1>Math Magicians</h1>
-      <ul className="nav-links">
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">Calculator</a>
-        </li>
-        <li>
-          <a href="#">Quote</a>
-        </li>
-      </ul>
-    </nav>
     <main className="quote-content">
       <h4>
         Mathematics as an expression of the human mind reflects the active
