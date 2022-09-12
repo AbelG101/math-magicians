b14aeb6ea1454caaf12c4af3764c6bc34b696e58
AbelG101 <abelgebeyehu101@gmail.com>
2022-09-12 23:24:07 +0300

    add component code for Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,33 @@
 import React from 'react';
-const Home = () => {
-  return (  );
-}
+
+const Home = () => (
+  <>
+    <nav className="nav-container">
+      <h1>Math Magicians</h1>
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Calculator</a></li>
+        <li><a href="#">Quote</a></li>
+      </ul>
+    </nav>
+    <main className="home-content">
+      <header className="content-header">
+        <h2>Welcome to our page!</h2>
+      </header>
+      <p className="content-paragraph">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam iure  reprehenderit.
+        Animi maxime iure dolor molestiae, aliquam blanditiis voluptatibus veritatis quae quia ipsa!
+        At reiciendis.
+        Minima sint labore sed debitis sunt error iste dolor ullam blanditiis, minus nihil.
+        Voluptates at.
+        Minima sint labore sed debitis sunt error iste dolor ullam blanditiis, minus nihil.
+        Voluptates at.
+        Minima sint labore sed debitis sunt error iste dolor ullam blanditiis, minus nihil.
+        Voluptates at., tempora tempore ex.
+        Non iste obcaecati doloremque hic tenetur iusto, modi ab sunt soluta velit. , illum velit?
+      </p>
+    </main>
+  </>
+);
 
 export default Home;
